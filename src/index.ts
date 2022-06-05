e4fbdca12a0ef948f9179fe2083eb8d525bedaf6
Leoniebra <leoniekrenzer96@gmail.com>
2022-06-05 18:56:05 +0200

    Added Prisma for Database connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-const config = require('../config.json');
-import Discord from 'discord.js';
+const config = require('./../config.json');
+const Discord = require('discord.js');
+import { MessageHandler } from './Handler/MessageHandler';
+import { PrismaClient, user } from '@prisma/client';
+
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const messageHandler = new MessageHandler();
+const prisma = new PrismaClient();
 
 client.on("messageCreate", function(message) { 
-    console.log(message);                                         
+    messageHandler.handle(message);
+    
+    main().catch((e) => { 
+        throw e
+    })
+    .finally(async () => {
+    await prisma.$disconnect();
+    });                                        
 }); 
+
+async function main() {
+    const allUsers = await prisma.user.findMany();
+    console.log(allUsers);
+}
 
 client.login(config.BOT_TOKEN);
